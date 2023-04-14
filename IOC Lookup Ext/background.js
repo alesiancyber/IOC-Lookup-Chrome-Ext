bbe596cfa5a2e0f664e7c3eefe5ceb098d0ccb02
alesiancyber <130629928+alesiancyber@users.noreply.github.com>
2023-04-13 20:29:24 -0700

    Update background.js

--- IOC Lookup Ext/background.js
+++ IOC Lookup Ext/background.js
@@ -1,9 +1,13 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.contextMenus.create({
-    id: "LookupIOC",
-    title: "Lookup IOC",
-    contexts: ["selection"],
-  });
+  try {
+    chrome.contextMenus.create({
+      id: "LookupIOC",
+      title: "Lookup IOC",
+      contexts: ["selection"],
+    });
+  } catch (e) {
+    console.error("Error creating context menu:", e);
+  }
 });
 
 function executeContentScript(tabId) {
@@ -14,7 +18,7 @@ function executeContentScript(tabId) {
     },
     () => {
       if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
+        console.error('Error:', chrome.runtime.lastError.message);
         return;
       }
       chrome.tabs.sendMessage(tabId, { command: "LookupIOC" });
@@ -24,12 +28,6 @@ function executeContentScript(tabId) {
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "LookupIOC") {
-    chrome.tabs.sendMessage(tab.id, { command: "ping" }, (response) => {
-      if (chrome.runtime.lastError) {
-        executeContentScript(tab.id);
-      } else {
-        chrome.tabs.sendMessage(tab.id, { command: "LookupIOC" });
-      }
-    });
+    executeContentScript(tab.id);
   }
 });
